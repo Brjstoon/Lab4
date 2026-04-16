6dd21d6856070e12c50cb182440ccc828f50021e
sami <sami@mail.com>
2026-04-16 11:21:01 +0300

    First

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,49 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+
+        //-------Circle Test---------//
+        Circle c = new Circle(6.5);
+        System.out.println( "Initial Radius: " + c.getRadius());
+        System.out.println( "The Area for a circle with 6.5 radius: "+ String.format("%.2f" , c.calculateArea()));
+        System.out.println("The Circumference for a circle with 6.5 radius: " + String.format("%.2f" , c.calculateCircumference()));
+        c.setRadius(8);
+        System.out.println("Setting the radius to 8.");
+        System.out.println( "The Area for a circle with 8 radius: "+ String.format("%.2f" , c.calculateArea()));
+        System.out.println("The Circumference for a circle with 8 radius: " + String.format("%.2f" , c.calculateCircumference()));
+
+
+        System.out.println("\n");
+
+
+        //--------Rectangle Test---------//
+        Rectangle r = new Rectangle(3 , 4);
+        System.out.println("The Area of a Rectangle with " + r.getWidth() + " Width and " + r.getHeight() + " Height = " + String.format("%.2f" , r.calculateArea()));
+        System.out.println("The Perimeter of a Rectangle with " + r.getWidth() + " Width and " + r.getHeight() + " Height = " + String.format("%.2f" , r.calculateCircumference()));
+        r.setHeight(5);
+        r.setWidth(6);
+        System.out.println("The Area of a Rectangle with " + r.getWidth() + " Width and " + r.getHeight() + " Height = " + String.format("%.2f" , r.calculateArea()));
+        System.out.println("The Perimeter of a Rectangle with " + r.getWidth() + " Width and " + r.getHeight() + " Height = " + String.format("%.2f" , r.calculateCircumference()));
+
+
+        System.out.println("\n");
+
+        //--------Triangle Test---------//
+        Triangle t = new Triangle(7 , 9);
+        System.out.println("The Area of a Triangle with " + t.getBase() + " Width and " + t.getHeight() + " Height = " + String.format("%.2f" , t.calculateArea()));
+        System.out.println("The Perimeter of a Triangle with " + t.getBase() + " Width and " + t.getHeight() + " Height = " + String.format("%.2f" , t.calculateCircumference()));
+        t.setHeight(5);
+        t.setBase(6);
+        System.out.println("The Area of a Triangle with " + t.getBase() + " Width and " + t.getHeight() + " Height = " + String.format("%.2f" , t.calculateArea()));
+        System.out.println("The Perimeter of a Triangle with " + t.getBase() + " Width and " + t.getHeight() + " Height = " + String.format("%.2f" , t.calculateCircumference()));
+
+
+
+
+
+
+
     }
 }
